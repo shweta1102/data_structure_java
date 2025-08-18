a980aba6d9d6501cf75b09f04335e6d7165f82b8
shweta1102 <shweta.agrawal2901@gmail.com>
2025-08-18 16:35:28 +0530

    intervals

---
+++ src/java/main/intervals/CanAttendMeetings.java
@@ -0,0 +1,44 @@
+package src.java.main.intervals;
+
+import java.util.Arrays;
+
+/**
+ * Write a function to check if a person can attend all the meetings scheduled without any time conflicts. Given an array intervals, where each element [s1, e1] represents a meeting starting at time s1 and ending at time e1, determine if there are any overlapping meetings. If there is no overlap between any meetings, return true; otherwise, return false.
+ * Note that meetings ending and starting at the same time, such as (0,5) and (5,10), do not conflict.
+ * Input:
+ * intervals = [(1,5),(3,9),(6,8)]
+ * Output:
+ * false
+ * Explanation: The meetings (1,5) and (3,9) overlap.
+ * Input:
+ * intervals = [(10,12),(6,9),(13,15)]
+ * Output:
+ * true
+ * Explanation: There are no overlapping meetings, so the person can attend all.
+ */
+public class CanAttendMeetings {
+
+    /**
+     * A person can attend all meetings if and only if none of the meetings overlap.
+     * By sorting the intervals by start time, we can easily check if any two consecutive intervals overlap.
+     * We iterate over each interval, beginning with the second interval in the sorted list.
+     * We compare the start time of the current interval with the end time of the previous interval.
+     * If the start time of the current interval is less than the end time of the previous interval, then the two intervals overlap and the person cannot attend both meetings, so we return false.
+     * Otherwise, the person can attend both meetings, and we continue to the next interval.
+     * If we reach the end of the list without finding any overlapping intervals, then the person can attend all meetings, and we return true.
+     *
+     * @param intervals
+     * @return
+     */
+    public Boolean canAttendMeetings(int[][] intervals) {
+        if (intervals.length <= 1)
+            return true;
+
+        Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1])
+                return false;
+        }
+        return true;
+    }
+}
---
+++ src/java/main/slidingwindow/MaximumSumOfDistinctSubArraysWithLengthK.java
@@ -0,0 +1,75 @@
+package src.java.main.slidingwindow;
+
+import java.util.HashMap;
+
+/**
+ * You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
+ * <p>
+ * The length of the subarray is k, and
+ * All the elements of the subarray are distinct.
+ * Return the maximum subarray sum of all the subarrays that meet the conditions. If no subarray meets the conditions, return 0.
+ * <p>
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,5,4,2,9,9,9], k = 3
+ * Output: 15
+ * Explanation: The subarrays of nums with length 3 are:
+ * - [1,5,4] which meets the requirements and has a sum of 10.
+ * - [5,4,2] which meets the requirements and has a sum of 11.
+ * - [4,2,9] which meets the requirements and has a sum of 15.
+ * - [2,9,9] which does not meet the requirements because the element 9 is repeated.
+ * - [9,9,9] which does not meet the requirements because the element 9 is repeated.
+ * We return 15 because it is the maximum subarray sum of all the subarrays that meet the conditions
+ * Example 2:
+ * <p>
+ * Input: nums = [4,4,4], k = 3
+ * Output: 0
+ * Explanation: The subarrays of nums with length 3 are:
+ * - [4,4,4] which does not meet the requirements because the element 4 is repeated.
+ * We return 0 because no subarrays meet the conditions.
+ * <p>
+ * Input: [3,2,3,1], k=3
+ * Output: 6
+ * <p>
+ * Input: [1,1,1,1,1,1], k=1
+ * Output: 1
+ * <p>
+ * Input: [9,9,9,1,2,3], k=3
+ * Output: 12
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= k <= nums.length <= 105
+ * 1 <= nums[i] <= 105
+ */
+public class MaximumSumOfDistinctSubArraysWithLengthK {
+    public long maximumSubarraySum(int[] nums, int k) {
+        //cannot use hashset because we need to keep count of repeating characters ex:[9,9,9,1,2,3] --> using set will fail this usecase when we slide the window over repeating character
+        //HashMap will also count the unique numbers int he array and ensure number equal to k when map size is k and as we keep removing the elements that are out of window --> if the map size == k then it will make sure that frequency of each element is 1
+        HashMap<Integer, Integer> state = new HashMap<Integer, Integer>();
+        int start = 0;
+        long maxSum = 0;
+        long currentSum = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            currentSum += nums[end];
+            state.put(nums[end], state.getOrDefault(nums[end], 0) + 1);
+
+            if (end - start + 1 == k) {
+                if (state.size() == k) {
+                    maxSum = Math.max(maxSum, currentSum);
+                }
+                state.put(nums[start], state.get(nums[start]) - 1);
+                currentSum -= nums[start];
+                if (state.get(nums[start]) == 0)
+                    state.remove(nums[start]);
+                start++;
+            }
+        }
+        return maxSum;
+    }
+}
